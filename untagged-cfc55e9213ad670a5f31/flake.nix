0aabdc04c8326afbd1605b989c8fed95a0991a51
hugosenari <hugosenari@gmail.com>
2022-03-24 20:25:26 -0300

    feat(nimpkgs): remove lock

--- untagged-cfc55e9213ad670a5f31/flake.nix
+++ untagged-cfc55e9213ad670a5f31/flake.nix
@@ -25,10 +25,13 @@
   let 
     lib  = flakeNimbleLib.lib;
     args = ["self" "nixpkgs" "flakeNimbleLib" "src-markx-untagged-cfc55e9213ad670a5f31"];
-  in lib.mkRefOutput {
+    over = if builtins.pathExists ./override.nix 
+           then { override = import ./override.nix; }
+           else { };
+  in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
     src  = deps."src-markx-untagged-cfc55e9213ad670a5f31";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
-  };
+  } );
 }
